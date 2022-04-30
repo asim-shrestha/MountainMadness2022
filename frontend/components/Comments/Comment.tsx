bf9c193c89c422952a2ae90c7757e105904c6cdf
Dusan Kovacevic <dkovacev@sfu.ca>
2022-04-30 09:55:48 -0700

    Attempted to add api call

--- frontend/components/Comments/Comment.tsx
+++ frontend/components/Comments/Comment.tsx
@@ -23,35 +23,9 @@ const comment = (videoListInfo) => {
       isShortened = "none";
 
     }
+    console.log(isShortened )
     
     return (
-      // <div id="description" className={"d-flex flex-column "}>
-      //   <div id="user-section" className={"d-flex justify-content-between"}>
-      //     <div id="user-info" className={"d-flex"}>
-      //       <img className={"rounded-circle"} alt="avatar" src={thumbnail} height={"50px"}/>
-      //       <div className={"d-flex flex-column m-2"}>
-      //         <h6 className={"font-weight-bold"}>{author}</h6>
-      //         {/* <h6 className={"text-secondary"} style={{fontSize: "0.80rem"}}>{userMetadata.subscribers} subscribers</h6> */}
-      //       </div>
-      //     </div>
-      //     {/* <div>
-      //       <button type="button" className="btn text-uppercase" style={{ color: "white", backgroundColor: "#CC0000"}}>Subscribe</button>
-      //     </div> */}
-      //   </div>
-
-      //   <div className="ml-5 mr-4 mt-4" style={{}}>
-      //     <h6>
-      //       { isShowMore ?  text.substring(0, 100) + "..." : text}
-      //     </h6>
-      //     <ShowMoreButton
-      //       className={(!isShowMore ? "mt-4" : "mt-3") +  " text-secondary text-uppercase"}
-      //       style={{fontSize: "0.75rem", color:"darkgray", }}
-      //       onClick={() => setIsShowMore(!isShowMore)}
-      //     >
-      //       Show {isShowMore ? "more" : "less"}
-      //     </ShowMoreButton>
-      //   </div>
-      // </div>
       <div id="description" className={"d-flex flex-column "}>
       <div id="user-section" className={"d-flex justify-content-between"}>
         <div id="user-info" className={"d-flex"}>
@@ -67,9 +41,6 @@ const comment = (videoListInfo) => {
               <small style={{paddingLeft:"10px", fontSize:"11px"}}>
                {publishedAt.substring(0,10)}{" "}
               </small>
-              {/* <h6 className={"text-secondary"} style={{ fontSize: "0.80rem" }}>
-                {userMetadata.subscribers} subscribers
-              </h6> */}
             </div>
             <div className="mr-2 mt-2" >
               <h6>
@@ -96,7 +67,6 @@ const comment = (videoListInfo) => {
           </div>
         </div>
       </div>
-
     </div>
     )
   }
--- frontend/components/Comments/CommentThread.tsx
+++ frontend/components/Comments/CommentThread.tsx
@@ -2,10 +2,6 @@ import axios from "axios"
 import { FunctionComponent, useState } from "react";
 import VideoMetadata from "../../interface/VideoMetadata";
 import Comment from "./Comment"
-// GET https://youtube.googleapis.com/youtube/v3/commentThreads?part=id%2Csnippet%2Creplies&videoId=7eX9Sa2zz0E&access_token=AIzaSyDZo7zVd48-X23NFN_KpmyxNXIavSWJnJc&key=[YOUR_API_KEY] HTTP/1.1
-
-// Authorization: Bearer [YOUR_ACCESS_TOKEN]
-// Accept: application/json
 
 type VideoComponentProps = {
     video: VideoMetadata;
@@ -13,14 +9,19 @@ type VideoComponentProps = {
   
 const CommentThread: FunctionComponent<VideoComponentProps> = ({ video }) => {
     // const [theme, setTheme] = useTheme()
-    const isDark = 'dark'
-    const data = [{"name":"test1", "description":"moew"},{"name":"test2", "description":"mix"},{"name":"test3", "description":"meowmix"}]
-
-    var data2 = require('./commentThread.json')  
     
+    
+    var data2 = require('./commentThread.json')  
+    var vidId = "7eX9Sa2zz0E"
     var temp = data2["items"];
-
-
+    var temp2 = [{"id":"test"}]
+    // console.log("meowmix")
+    const res = axios.get(`https://www.googleapis.com/youtube/v3/commentThreads?part=id%2Csnippet&maxResults=20&videoId=7eX9Sa2zz0E&key=AIzaSyDZo7zVd48-X23NFN_KpmyxNXIavSWJnJc`).then(
+      response => {
+        temp2 = response.data["items"];
+      }
+    )
+    // console.log("kkkeke")
     return (
       <div>
           {temp.map(function(d, idx){
