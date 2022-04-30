d3e36d006efb6570cffeddf5745f03df7a4f568a
Jennifer Dai <jydai@sfu.ca>
2022-04-30 10:42:50 -0700

    comment api request

--- frontend/components/Comments/CommentThread.tsx
+++ frontend/components/Comments/CommentThread.tsx
@@ -1,35 +1,48 @@
-import axios from "axios"
-import { FunctionComponent, useState } from "react";
+import axios from "axios";
+import { FunctionComponent, useState, useEffect } from "react";
 import VideoMetadata from "../../interface/VideoMetadata";
-import Comment from "./Comment"
+import Comment from "./Comment";
 
 type VideoComponentProps = {
-    video: VideoMetadata;
-  };
-  
+  video: VideoMetadata;
+};
+
 const CommentThread: FunctionComponent<VideoComponentProps> = ({ video }) => {
-    // const [theme, setTheme] = useTheme()
-    
-    
-    var data2 = require('./commentThread.json')  
-    var vidId = "7eX9Sa2zz0E"
-    var temp = data2["items"];
-    var temp2 = [{"id":"test"}]
-    // console.log("meowmix")
-    const res = axios.get(`https://www.googleapis.com/youtube/v3/commentThreads?part=id%2Csnippet&maxResults=20&videoId=7eX9Sa2zz0E&key=AIzaSyDZo7zVd48-X23NFN_KpmyxNXIavSWJnJc`).then(
-      response => {
-        temp2 = response.data["items"];
-      }
+  // const [theme, setTheme] = useTheme()
+  const [allComments, setAllComments] = useState([]);
+
+  var data2 = require("./commentThread.json");
+  var vidId = "7eX9Sa2zz0E";
+  var temp = data2["items"];
+  var temp2 = [{ id: "test" }];
+  // console.log("meowmix")
+  /*const res = await axios
+    .get(
+      `https://www.googleapis.com/youtube/v3/commentThreads?part=id%2Csnippet&maxResults=20&videoId=7eX9Sa2zz0E&key=AIzaSyDZo7zVd48-X23NFN_KpmyxNXIavSWJnJc`
     )
-    // console.log("kkkeke")
-    return (
-      <div>
-          {temp.map(function(d, idx){
-              return (<Comment videoListInfo={d.snippet.topLevelComment.snippet}/>)
-          })}
-      
-      </div>
-    )
-  }
-  
-  export default CommentThread
+    .then((response) => {
+      temp2 = response.data["items"];
+      console.log(temp2);
+    });*/
+
+  useEffect(() => {
+    axios
+      .get(
+        `https://www.googleapis.com/youtube/v3/commentThreads?part=id%2Csnippet&maxResults=20&videoId=${vidId}&key=AIzaSyDZo7zVd48-X23NFN_KpmyxNXIavSWJnJc`
+      )
+      .then((response) => {
+        const temp2 = response.data["items"];
+        setAllComments(temp2);
+      });
+  }, []);
+  // console.log("kkkeke")
+  return (
+    <div>
+      {allComments.map(function (d, idx) {
+        return <Comment videoListInfo={d.snippet.topLevelComment.snippet} />;
+      })}
+    </div>
+  );
+};
+
+export default CommentThread;
