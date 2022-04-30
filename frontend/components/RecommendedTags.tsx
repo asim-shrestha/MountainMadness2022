163abb79410ba11ff23545503b2031b572c1bbf9
Jennifer Dai <jydai@sfu.ca>
2022-04-30 12:01:22 -0700

    spacing between tags and recommendations

--- frontend/components/RecommendedTags.tsx
+++ frontend/components/RecommendedTags.tsx
@@ -4,33 +4,47 @@ import Description from "./Description";
 import HoverCursorDiv from "./HoverCursorDiv";
 import VideoHud from "./VideoHud";
 
-const tags = ["All", "Tubes", "TuBeS", "TUBES", "TOOBS", "TUBeZ", "toobs", "tOObs"];
+const tags = [
+  "All",
+  "Tubes",
+  "TuBeS",
+  "TUBES",
+  "TOOBS",
+  "TUBeZ",
+  "toobs",
+  "tOObs",
+];
 
 const RecommendedTags: FunctionComponent = () => {
-	const [tagIndex, setTagIndex] = useState(0);
+  const [tagIndex, setTagIndex] = useState(0);
 
-	return (
-		<div className="d-flex" style={{gap: "0.25rem", overflow: "hidden"}}>
-			{tags.map((tag, idx) =>
-				<HoverCursorDiv
-					className={(tagIndex == idx ? "bg-dark text-white border border-dark" : "")}
-					id={tag}
-					style={{
-						borderRadius: "3rem",
-						padding: "0.3rem 0.75rem 0.3rem 0.75rem",
-						fontSize: "0.8rem",
-						backgroundColor: "#ECECEC",
-						borderStyle: "solid",
-						borderWidth: "1px",
-						borderColor: "#dadada",
-					}}
-					onClick={() => setTagIndex(idx)}
-				>
-					{tag}
-				</HoverCursorDiv>
-			)}
-		</div>
-	);
+  return (
+    <div
+      className="d-flex"
+      style={{ gap: "0.25rem", overflow: "hidden", marginBottom: "1em" }}
+    >
+      {tags.map((tag, idx) => (
+        <HoverCursorDiv
+          className={
+            tagIndex == idx ? "bg-dark text-white border border-dark" : ""
+          }
+          id={tag}
+          style={{
+            borderRadius: "3rem",
+            padding: "0.3rem 0.75rem 0.3rem 0.75rem",
+            fontSize: "0.8rem",
+            backgroundColor: "#ECECEC",
+            borderStyle: "solid",
+            borderWidth: "1px",
+            borderColor: "#dadada",
+          }}
+          onClick={() => setTagIndex(idx)}
+        >
+          {tag}
+        </HoverCursorDiv>
+      ))}
+    </div>
+  );
 };
 
 export default RecommendedTags;
