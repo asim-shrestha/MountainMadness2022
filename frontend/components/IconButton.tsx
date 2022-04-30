7c0be32dfdbd31851c3221744c30bdbfedae08f5
Asim Shrestha <ashresth@sfu.ca>
2022-04-30 14:42:13 -0700

    feat: fix issues

--- frontend/components/IconButton.tsx
+++ frontend/components/IconButton.tsx
@@ -10,7 +10,7 @@ const IconButton: FunctionComponent<IconButtonProps> = ({iconUrl, text, normal})
 	return (
 		<div className="d-flex align-items-center">
 			{ iconUrl == "" ? "" : <img alt="icon-button" src={iconUrl} height="25px"/> }
-			<p className={(normal ? "" : "font-weight-bold") + " m-2 mr-3"} style={{fontSize: "0.85rem"}}>{text.toUpperCase()}</p>
+			<p className={(normal ? "" : "font-weight-bold") + " m-2 mr-3"} style={{fontSize: "0.85rem"}}>{text?.toUpperCase()}</p>
 		</div>
 	)
 }
